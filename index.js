15e01e811f8df4bc12eeedfa8a914adad4c019de
zchips <81702433+zchips@users.noreply.github.com>
2022-11-21 15:10:20 -0700

    test run

--- index.js
+++ index.js
@@ -77,11 +77,11 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(age){
+ return age * 7 
 }
 
-
+console.log(dogYears(1))
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
